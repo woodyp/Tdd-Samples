b878947b8b1ce7f60fb71d5217cf6392b0195f9f
woodyp <woody@pewitt.org>
2014-05-01 22:12:30 -0700

    First check in after demo

---
+++ TDD-Samples-Tests/CalcTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_Samples_Tests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void Add_1plus1_Expect2()
+        {
+            TDD_Samples.Calc c = new TDD_Samples.Calc();
+
+            long r = c.Add(1, 1);
+
+            Assert.AreEqual(r, 2);
+        }
+
+        [TestMethod]
+        public void Add_4plus4_Expect8()
+        {
+            TDD_Samples.Calc c = new TDD_Samples.Calc();
+
+            long r = c.Add(4, 4);
+
+            Assert.AreEqual(r, 8);
+        }
+
+        [TestMethod]
+        public void Div_4Divided2_Expect2()
+        {
+            TDD_Samples.Calc c = new TDD_Samples.Calc();
+
+            double r = c.Div(4, 2);
+
+            Assert.AreEqual(r, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void Div_DividedBy0_ExpectException()
+        {
+            TDD_Samples.Calc c = new TDD_Samples.Calc();
+
+            int r = c.Div(2, 0);          
+        }
+
+        [TestMethod]
+        public void Divd_DividedBy0_ExpectInfinity()
+        {
+            TDD_Samples.Calc c = new TDD_Samples.Calc();
+
+            double r = c.Divd(2, 0);
+
+            Assert.IsTrue(double.IsInfinity(r));
+        }
+
+    }
+}
---
+++ TDD-Samples/Calc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Samples
+{
+    public class Calc
+    {
+        public long Add(int p1, int p2)
+        {
+            return p1 + p2;
+        }
+
+        public int Div(int p1, int p2)
+        {
+            return p1 / p2;       
+        }
+
+        public double Divd(double p1, double p2)
+        {
+            return p1 / p2;
+        }
+    }
+}
